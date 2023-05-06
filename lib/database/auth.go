fd77793d01bb4bfb55c3ee2a4f76686272d56815
Aryo Bimo <abimo2018@gmail.com>
2023-05-07 01:55:14 +0700

    Merubah url pada route

--- lib/database/auth.go
+++ lib/database/auth.go
@@ -9,16 +9,13 @@ import (
 
 func Login(req *payload.LoginForm) (user models.User, err error) {
 	user = models.User{}
-
 	if err = config.DB.Where("username = ? OR email = ? AND password = ?", req.ID, req.ID, req.Password).First(&user).Error; err != nil {
 		return user, err
 	}
-
 	return
 }
 
 func Register(user *models.User) error {
-
 	if err := config.DB.Save(&user).Error; err != nil {
 		return err
 	}
