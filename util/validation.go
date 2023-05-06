b1b9915759e5096b6228adda91c778c9b078f4b5
Aryo Bimo <abimo2018@gmail.com>
2023-05-07 04:50:57 +0700

    Memperbaiki validation

--- util/validation.go
+++ util/validation.go
@@ -1,11 +1,19 @@
 package util
 
-import "github.com/go-playground/validator/v10"
+import (
+	"net/http"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/labstack/echo"
+)
 
 type CustomValidator struct {
 	Validator *validator.Validate
 }
 
 func (cv *CustomValidator) Validate(i interface{}) error {
-	return cv.Validator.Struct(i)
+	if err := cv.Validator.Struct(i); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+	return nil
 }
