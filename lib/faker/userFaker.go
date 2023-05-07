c402c952e66ae3aab84cb3a0d14920ffbcc02622
Aryo Bimo <abimo2018@gmail.com>
2023-05-07 16:56:11 +0700

    Melakukan hashing pada seeder

--- lib/faker/userFaker.go
+++ lib/faker/userFaker.go
@@ -4,14 +4,16 @@ import (
 	"mini-project/models"
 
 	"github.com/jinzhu/gorm"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func UserFaker(db *gorm.DB) *models.User {
+	password, _ := bcrypt.GenerateFromPassword([]byte("admin"), bcrypt.DefaultCost)
 	return &models.User{
 		Name:     "admin",
 		Username: "admin",
 		Email:    "admin",
-		Password: "admin",
+		Password: string(password),
 		Role:     "admin",
 	}
 }
